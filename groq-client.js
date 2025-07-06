b3783e1cb150b62518d5e4632777e5ff554db419
hakanttek <hakanttek@gmail.com>
2025-07-06 04:05:53 +0200

    Add send function to groq-client for chat completions

--- groq-client.js
+++ groq-client.js
@@ -19,4 +19,16 @@ async function getGroqChatCompletion() {
   });
 }
 
-module.exports = { main, getGroqChatCompletion };
+async function send(content, options = undefined) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: options.role ?? "user",
+        content: content,
+      },
+    ],
+    model: options.model ?? process.env.DEFAULT_MODEL
+  });
+}
+
+module.exports = { main, getGroqChatCompletion, send };
